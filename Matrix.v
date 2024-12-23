140e3c3a137b65d05f8ddd706b08155a8498c171
HiraIrshad <hirairshad236@gmail.com>
2024-12-23 14:53:54 +0500

    Add files via upload

---
+++ Matrix.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module Matrix(
+	input rst,
+	output reg [26:0] mat_out
+    );
+
+wire [2:0] mat [0:8];
+
+assign 	mat[0] = 3'd 1;
+assign 	mat[1] = 3'd 2;
+assign	mat[2] = 3'd 3;
+assign	mat[3] = 3'd 3;
+assign	mat[4] = 3'd 2;
+assign	mat[5] = 3'd 1;
+assign	mat[6] = 3'd 1;
+assign	mat[7] = 3'd 2;
+assign	mat[8] = 3'd 3;
+
+always @ (posedge rst) begin
+	mat_out = {mat[8], mat[7], mat[6], mat[5], mat[4], 
+              mat[3], mat[2], mat[1], mat[0]};
+end
+
+endmodule
